4e4f839abe291f2378adad92179c71eb86c7733d
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-10-10 11:51:27 +0300

    start project

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,9 @@ import { testState } from "../recoilState/recoilState";
 const Home: NextPage = (): JSX.Element => {
   const [state, setState] = useRecoilState(testState);
   return (
-    <div className="flex min-h-screen bg-violet-100  flex-col items-center justify-center py-2 ">
-      <DarkModeToggle />
-      <div className=" flex flex-col relative items-center border-y-2 border-y-red-500 py-4">
-        <div>Recoil state: {state.toString()}</div>
-        <div className=" text-red-500 text-5xl font-kufiM">PugaMuga</div>
-        <div className=" text-white text-5xl font-nablaPixel bg-slate-900 px-4 py-2 my-2">
-          PugaMuga
-        </div>
-        <div className=" text-red-500 text-5xl font-strongItalik">PugaMuga</div>
-        <div className=" text-red-500 text-5xl font-lobster">PugaMuga</div>
-        <div className=" absolute top-[-13px] right-[-21px] rotate-90 hover:rotate-0 tr-700">
-          <SvgIcon onClick={() => {
-            setState(!state)
-          }}/>
-        </div>
+    <div className="p-12">
+      <div className=" h-96 w-96 rounded-lg bg-black">
+
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       strongItalik: ["Ms Madi", "cursive"],
       lobster: ["Lobster", "cursive"],
     },
+    colors: {
+      primary: "##13003f",
+    },
   },
   plugins: [],
 };
