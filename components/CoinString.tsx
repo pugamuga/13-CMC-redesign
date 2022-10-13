2942cc79e113bb92bce6436eca4fe1f4385227e2
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-10-13 11:49:21 +0300

    add moment js

--- components/CoinString.tsx
+++ components/CoinString.tsx
@@ -18,33 +18,58 @@ export default function CoinString({ coin }: IProps): JSX.Element {
   console.log(sparklineColor);
 
   return (
-    <div className=" w-full grad-50 py-2 px-4 rounded-md flex items-center">
-      <div className=" flex items-center space-x-3 w-[20%] ">
+    <div className=" w-full grad-50 py-2 px-2 md:px-4 rounded-md flex items-center">
+      {/* ----------------------- */}
+      <div
+        className=" flex items-center space-x-3 md:w-[20%] w-[40%] "
+        id="name"
+      >
         {likeCoin ? <AiFillStar /> : <AiOutlineStar />}
-        <p className="text-sm text-center">{coin.market_cap_rank}</p>
+        <p className="md:text-sm text-xs text-center">{coin.market_cap_rank}</p>
         <div className="mx-2 superflex  relative ">
           <img
             src={coin.image}
-            className="h-6 w-6 object-contain rounded-full "
+            className="md:h-6 md:w-6 h-4 w-4 object-contain rounded-full "
           />
         </div>
-        <p className="text-sm">{coin.name}</p>
+        <p className="md:text-sm text-xs">{coin.name}</p>
       </div>
-      <div className="w-[40%] flex items-center justify-between px-12">
-        <p>{coin.symbol.toUpperCase()}</p>
-        <div className="  superflex">{coin.current_price}</div>
-        <div className={`${coin.price_change_percentage_24h>0?"text-[#67dfbd]":"text-[#ff7171]"}`}>
+      {/* ----------------------- */}
+      <div
+        className="md:w-[40%] w-[30%] flex items-center justify-center md:justify-between md:px-12 "
+        id="price"
+      >
+        <p className=" md:inline hidden">{coin.symbol.toUpperCase()}</p>
+        <div className="  superflex">
+          <span className="text-xs text-white/50">$</span>
+          {coin.current_price}
+        </div>
+        <div
+          className={`${
+            coin.price_change_percentage_24h > 0
+              ? "text-[#67dfbd]"
+              : "text-[#ff7171]"
+          } md:inline hidden`}
+        >
           {coin.price_change_percentage_24h.toFixed(2)}%
         </div>
       </div>
-      <div className=" flex h-full w-[40%] items-center justify-between px-2">
-        <div className="md:truncate md:w-[35%] md:inline hidden">${coin.market_cap}</div>
-        <div className={`w-[50%] h-full ml-4`}>
+      {/* ----------------------- */}
+      <div
+        className=" flex h-full w-[30%] md:w-[40%] items-center justify-end md:justify-between px-2 md:scale-100 scale-110  "
+        id="sparkline"
+      >
+        <div className="md:truncate md:w-[35%] md:inline hidden">
+          <span className="text-xs text-white/50 md:ml-12">$</span>
+          {coin.market_cap}
+        </div>
+        <div className={`md:w-[50%] w-full h-full md:ml-4`}>
           <Sparklines data={coin.sparkline_in_7d.price}>
             <SparklinesLine color={sparklineColor} />
           </Sparklines>
         </div>
       </div>
+      {/* ----------------------- */}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import { useRecoilState } from "recoil";
 import CoinString from "../components/CoinString";
 import { coinDataState } from "../recoilState/recoilState";
 
-
 interface IProps {
   data: MainCoinData[] | [];
 }
@@ -18,16 +17,25 @@ const Home = ({ data }: IProps): JSX.Element => {
 
   useEffect(() => {
     setCoins(data);
-    
   }, []);
 
   return (
-    <div className="w-full">
-      <div className=" flex flex-col w-full space-y-2 items-center h-12 ">
-        {coins.slice(0, 20).map((coin: MainCoinData, id: number) => {
-          return <CoinString key={id} coin={coin} />;
-        })}
-      </div>
+    <div className="w-full ">
+        <div className=" flex w-full justify-end md:px-4">
+          {<>
+            <p className=" text-xs md:text-sm text-white/30">
+              Last price update:{" "}
+              {coins[0] && Array.from(coins[0]?.last_updated).slice(11, 19)} UTC
+            </p>
+            <p>{Date.UTC}</p>
+            </>
+          }
+        </div>
+        <div className=" flex flex-col w-full space-y-2 items-center h-12 ">
+          {coins.slice(0, 20).map((coin: MainCoinData, id: number) => {
+            return <CoinString key={id} coin={coin} />;
+          })}
+        </div>
     </div>
   );
 };
