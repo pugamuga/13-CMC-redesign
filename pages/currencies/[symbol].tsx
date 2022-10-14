48f8c332404e84951c974bd5a77e107ccc6789d7
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-10-14 16:52:06 +0300

    fix problems

--- pages/currencies/[symbol].tsx
+++ pages/currencies/[symbol].tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { GetStaticPropsContext } from "next";
 import Link from "next/link";
 import { IoIosArrowRoundBack } from "react-icons/io";
+import { useRecoilState } from "recoil";
+import { coinDataState } from "../../recoilState/recoilState";
 
 const coinGeckoUrl =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true";
@@ -23,34 +25,48 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
-  try {
-    if (context.params?.symbol) {
-      const responce = await fetch(coinGeckoUrl);
-      const data = await responce.json();
-
-      const coinData = context.params.symbol;
-
-      if (data) {
-        const arrayCoinsCheck: MainCoinData = data?.filter(
-          (coin: MainCoinData) => coin.symbol === coinData
-        );
-
-        return {
-          props: { coin: arrayCoinsCheck },
-        };
-      }
-    }
-  } catch (error) {
-    return null;
+  if (context.params?.symbol) {
+    const coinData = context.params.symbol;
+    return {
+      props: { coin: coinData },
+    };
   }
+
+  // try {
+  //   if (context.params?.symbol) {
+  //     const responce = await fetch(coinGeckoUrl);
+  //     const data = await responce.json();
+
+  //     const coinData = context.params.symbol;
+
+  //     if (data) {
+  //       const arrayCoinsCheck: MainCoinData = data?.filter(
+  //         (coin: MainCoinData) => coin.symbol === coinData
+  //       );
+
+  //       return {
+  //         props: { coin: arrayCoinsCheck },
+  //       };
+  //     }
+  //   }
+  // } catch (error) {
+  //   return null;
+  // }
 };
 
 interface IProps {
   coin: MainCoinData[];
 }
 
-export default function CoinPage({ coin }: IProps): JSX.Element {
-  console.log(coin)
+export default function CoinPage({ coin }: any): JSX.Element {
+  const [coins, setCoins] = useRecoilState(coinDataState);
+
+  const coinDataOnPage: MainCoinData | MainCoinData[] = coins.filter(
+    (coinName: MainCoinData) => coinName.symbol === coin
+  );
+  const coinUse: MainCoinData = coinDataOnPage[0];
+
+  console.log(coinUse);
   if (coin) {
     return (
       <div>
